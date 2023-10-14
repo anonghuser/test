5495bf3f5ffad40e884cf8f3e75a95a8c5310155
anonghuser <110276125+anonghuser@users.noreply.github.com>
2023-10-15 02:39:55 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,13 @@
 console.log('in sw.js', location.href)
 
-self.addEventListener("activate", (event) => {
+addEventListener("activate", (event) => {
   event.waitUntil(clients.claim());
+});
+
+addEventListener("fetch", (event) => {
+  console.log('sw fetch', event);
+});
+
+addEventListener("message", (event) => {
+  console.log('sw message', event);
 });
