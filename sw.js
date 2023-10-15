2709e679a8b6a9eab30e8a2bcf6c4e5841165e38
anonghuser <110276125+anonghuser@users.noreply.github.com>
2023-10-15 04:48:40 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,8 @@
 console.log('in sw.js', location.href)
 
 addEventListener("activate", (event) => {
-  const claim = clients.claim();
-  event.waitUntil(claim);
-  claim.then(()=>registration.unregister());
+  clients.claim();
+  registration.unregister();
 });
 
 addEventListener("fetch", (event) => {
