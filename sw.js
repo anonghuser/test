15a347b0728ac4ed24493c28155a30daa72cd258
anonghuser <110276125+anonghuser@users.noreply.github.com>
2023-10-23 16:59:18 +0300

    test

--- sw.js
+++ sw.js
@@ -11,7 +11,9 @@ const activeStreams = []
 addEventListener("fetch", async (event) => {
   if (event.request.url.includes('fake')) {
     const client = await clients.get(event.clientId)
+    let cnt = 0
     client.postMessage({type: 'z', data: JSON.parse(JSON.stringify(event, (_, value) => {
+      if (cnt++ > 1000) return '???'
       if (typeof value == 'object') {
         const result = {}
         for (const key in value) result[key] = value[key]
