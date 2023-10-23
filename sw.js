a5fd12fb86b139467e6b6d3aab4b64aeb781a63a
anonghuser <110276125+anonghuser@users.noreply.github.com>
2023-10-23 18:46:17 +0300

    test

--- sw.js
+++ sw.js
@@ -33,7 +33,7 @@ const activeStreams = []
 addEventListener("fetch", async (event) => {
   if (event.request.url.includes('fake')) {
     const client = await clients.get(event.clientId)
-    client.postMessage(JSONify({type: 'z', data: event}, [globalThis]))
+    client.postMessage(JSONify({type: 'fetch', data: event}, [globalThis]))
     return
     const stream = new ReadableStream({
       start(controller) {
